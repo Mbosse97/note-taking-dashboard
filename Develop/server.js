7d87dffc56bb46852b3540520f8ae0ae637085d8
Michael B <bosse730@gmail.com>
2023-02-09 19:38:41 +1100

    linked routes to serverjs

--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,15 @@
-// check if link between pages is broken
-// create route to notes pages with get request
-// create get and post requests on the notes in notes page
+// check if link between pages is broken            DONE
+// create route to notes pages with get request         DONE
+// create get request to log existing notes on left
+// create post request to log new notes on the left when saved 
+// create get request when note is clicked to present on right
+// create get request from write button to link to notes.html
 
 const express = require('express');
 const path = require('path')
 const fs = require('fs')
-
+const api = require('./routes/index')
+const { readFromFile, writeToFile, readAndAppend } = require('./helpers/fsUtils')
 
 const PORT = 3001;
 
@@ -16,6 +20,8 @@ app.use(express.urlencoded({extended:true}));
 
 app.use(express.static('public'));
 
+app.use('/api',api);
+
 app.get('/notes', (req,res) => 
     res.sendFile(path.join(__dirname,'/public/notes.html'))
 );
@@ -24,3 +30,6 @@ app.get('*', (req,res) =>
     res.sendFile(path.join(__dirname,'/public/index.html'))
 );
 
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
